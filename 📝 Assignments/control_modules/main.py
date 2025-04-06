a99c0f29e51ea676acf95ff62c02552f72070d4e
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2025-04-06 21:33:25 +0500

    Add files via upload

---
+++ 📝 Assignments/control_modules/main.py
@@ -0,0 +1,8 @@
+# Control Module  (File containing related function)
+# A Seperate .py file that manages or controls functions or logic
+
+import math_control # This math_control.py has become a Control Module — the main logic is here, and main.py just calls it
+
+print(math_control.multiply(4, 3))
+print(math_control.subtract(7, 3))
+
---
+++ 📝 Assignments/control_modules/math_control.py
@@ -0,0 +1,5 @@
+def multiply(a, b):
+    return a * b
+
+def subtract(c, d):
+    return c - d
